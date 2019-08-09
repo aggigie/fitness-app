a816d5dcaf545e4b429cc10ff3a18c0395b20ece
aggigie <aggigie46@gmail.com>
2019-08-10 00:17:27 +0200

    Now deleting user and products works.

--- pal_app/urls.py
+++ pal_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('products/', views.show_products),
     path('', views.main),
     path('user-data/', views.user_data),
+    path('delete-account/', views.delete_user, name="delete_user"),
+    path('remove-product/<int:product_id>/', views.remove_product, name="remove_product"),
     path('meal/<int:meal_type>/', views.meal_data)
 ]
--- pal_app/views.py
+++ pal_app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 
 
 def show_products(request):
-    product_list = Product.objects.all()
+    product_list = Product.objects.all().order_by('name')
     context = {'product_list': product_list}
     return render(request, 'pal_app/products-list.html', context)
 
@@ -53,6 +53,15 @@ def calc_meal(meal_type):
     return value
 
 
+def delete_user(request):
+    user = User.objects.first()
+    user.delete()
+    meals = Meal.objects.filter(day=today())
+    for meal in meals:
+        meal.delete()
+    return redirect('/')
+
+
 def main(request):
     if User.objects.count() == 0:
         response = redirect('/user-data')
@@ -68,23 +77,30 @@ def main(request):
                    'date': today(),
                    'goal': goal,
                    'calories_left': int(goal - todays_intake),
-                   'cal_of_meals': meal_cal
+                   'cal_of_meals': meal_cal,
+                   'intake': todays_intake
                    }
         response = render(request, 'pal_app/fitness-app.html', context)
     return response
 
 
+def remove_product(request, product_id):
+    pr = Product.objects.get(id=product_id)
+    pr.delete()
+    return redirect('/products')
+
+
 def user_data(request):
+    msg = ""
     try:
         user_name = request.POST['user_name']
         height = request.POST['height']
         weight = request.POST['weight']
         age = request.POST['age']
         sex = request.POST['sex'] == 'm'
-        msg = ''
         if User.objects.count() == 0:
             ud = User(user_name=user_name, height=height, weight=weight, age=age, sex=sex)
-            msg = "Successfully added to database."
+            msg = "Welcome to FitnessApp!"
         else:
             ud = User.objects.first()
             ud.user_name = user_name
@@ -92,10 +108,13 @@ def user_data(request):
             ud.weight = weight
             ud.age = age
             ud.sex = sex
-            msg = "Successfully updated your information."
+            msg = "Successfully updated your data."
         ud.save()
     except KeyError:
-        return render(request, 'pal_app/user-data.html')
+        if User.objects.count() > 0:
+            ud = User.objects.first()
+            return render(request, 'pal_app/user-data.html', {
+                'user': ud})
     return render(request, 'pal_app/user-data.html', {
         'debug': msg
     })
